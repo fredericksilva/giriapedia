7518f8667892c3f9bbcf24940e63c76da5676598
Mario Idival <marioidival@gmail.com>
2015-04-13 00:50:25 -0300

    add ui-route instead ngRoute

--- giriapedia/static/js/app.js
+++ giriapedia/static/js/app.js
@@ -4,23 +4,52 @@
     .module(
       "GiriaPedia",
       [
-        "ngRoute",
-        "ui.bootstrap"
+        "ngAnimate",
+        "ui.bootstrap",
+        "ui.router",
+        "toastr",
       ]
     )
     .config(routePedia);
 
 
-  function routePedia($routeProvider) {
-    $routeProvider
-      .when('/', {
+  function routePedia($stateProvider, $urlRouterProvider, toastrConfig) {
+
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+      .state("home", {
+        url: "/",
         templateUrl: 'static/js/partials/map.html',
         controller: 'MapperController',
         controllerAs: 'vm'
       })
-      .otherwise({
-        redirectTo: '/'
+      .state("giriasEstado", {
+        url: "/:state/",
+        templateUrl: 'static/js/partials/girias_state.html',
+        controller: 'GiriasEstado',
+        controllerAs: 'vm'
       })
+      .state("login", {
+        url: "/login/",
+        templateUrl: "static/js/partials/login.html",
+        controller: "LoginCtrl",
+        controllerAs: "vm"
+      })
+      .state("signin", {
+        url: "/cadastrar/",
+        templateUrl: "static/js/partials/signin.html",
+        controller: "SignInCtrl",
+        controllerAs: "vm"
+      })
+      .state("logout", {
+        url: "/logout/",
+        controller: "LogoutCtrl",
+      });
+
+      angular.extend(toastrConfig, {
+        preventDuplicates: true
+      });
 
   }
 
