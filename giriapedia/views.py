5079359159472e33105bb1d190658e56e7088811
Mario Idival <marioidival@gmail.com>
2015-04-22 23:22:08 -0300

    Return a write comments, code and commit in english! sorry :(

--- giriapedia/views.py
+++ giriapedia/views.py
@@ -8,25 +8,26 @@ import jwt
 
 @view_config(route_name='home', renderer='templates/main.html')
 def my_view(request):
-    """View que retorna somente a pagina inicial"""
+    """View return start page"""
     return {}
 
 
 @view_config(route_name="estados", renderer="json")
-def estados(request):
-    """Retorna uma lista de codigos dos estados brasileiros, juntamente com uma
-    URL estatica, para ser usada com angular.js"""
+def states(request):
+    """Return list with brazilian state code, together an URL static for
+    angular.js """
     gif_static = request.static_url('giriapedia:static/img/null.gif')
-    estados = [{"code":state.code, "static": gif_static}
-                for state in State.objects.all()]
-    return estados
+    list_state = [
+        {"code":state.code, "static": gif_static}
+        for state in State.objects.all()
+    ]
+    return list_state
 
 
 @view_defaults(route_name="girias:collection", renderer="json")
 class GiriaCollectionView(object):
     """
-    View que tem responsabilidade de criar e retornar girias.
-    Respondem apenas a GET e POST.
+    View handler to GET and POST request, create and return `giria`
     """
 
     def __init__(self, request):
@@ -37,8 +38,8 @@ class GiriaCollectionView(object):
 
     @view_config(request_method="GET")
     def collection(self):
-        """Retorna uma lista de todas as girias. Se for passado por parametro um
-        estado, retorna todas as girias desse estado."""
+        """ Return list with all `girias`. If exists `current_state`, return all
+        `girias` from that `current_state`"""
         if self.current_state:
             state = State.objects(code=self.current_state).first()
             girias = Giria.objects(state=state).all()
@@ -47,13 +48,12 @@ class GiriaCollectionView(object):
             return girias
         return Giria.objects.all()
 
-
     @view_config(request_method="POST")
     def create(self):
-        """View responsavel por criar a giria. Se existir uma giria cadastrada
-        para esse estado, retorna um error para o front-end."""
-        giria = self.req.json
+        """View used to create a `giria`. If `giria` exists to `current_state`,
+        return an error to frontend."""
 
+        giria = self.req.json
         state = State.objects(code=giria["state"]).first()
         user = User.objects(token=self.token).first()
 
@@ -75,7 +75,7 @@ class GiriaCollectionView(object):
 
 @view_defaults(route_name="girias:items", renderer="json")
 class GiriaItemView(object):
-    """View responsavel por responder a uma giria espeficia"""
+    """View handler to specific `giria`."""
 
     def __init__(self, request):
         self.req = request
@@ -86,7 +86,7 @@ class GiriaItemView(object):
 
     @view_config(request_method="GET")
     def get_giria(self):
-        """Retorna uma giria espeficia. por `state` e `giria`"""
+        """Return `giria` instance specific, by `state` and `giria` argument"""
         state = State.objects(code=self.state).first()
         giria = Giria.objects(state=state, giria=self.giria).first()
 
@@ -96,17 +96,16 @@ class GiriaItemView(object):
 
     @view_config(request_method="PUT")
     def update_giria(self):
-        """View para atualizar uma giria. Porém é apenas usada para adicionar
-        novos significados para uma determinada giria - passada por parametro."""
+        """View to update `giria`. So this it's only used to add a new
+        `description` for a certain `giria`.  """
         state = State.objects(code=self.state).first()
         giria = Giria.objects(state=state, giria=self.giria).first()
 
-        # Salvando sem verificar descrição
+        # Save a `giria` without verification
         if giria:
             giria.description.append(
                 prepare_user_description(self.req.json)
             )
-
             giria.save()
 
             self.sort_description(giria)
@@ -116,48 +115,46 @@ class GiriaItemView(object):
 
     @view_config(request_method="PUT", request_param="upvotes=true")
     def send_upvote(self):
-        """View responsavel pelo upvotes de um significado.
-        + 1 nos votos e adiciona o token do usuario em uma lista, onde existem
-        usuarios que ja votaram nesse significado."""
-        indice = self.req.json['description']
+        """View handler to save upvotes from `description`.
+        + 1 in `votes` and add user `token` a list `users`, where have only users
+        that votes it."""
+        index_ = self.req.json['description']
 
         state = State.objects(code=self.state).first()
         giria = Giria.objects(state=state, giria=self.giria).first()
 
         if giria:
 
-            giria.description[indice].votes['votes'] += 1
-            giria.description[indice].votes['users'].append(self.token)
+            giria.description[index_].votes['votes'] += 1
+            giria.description[index_].votes['users'].append(self.token)
             giria.save()
 
             self.sort_description(giria)
-
             return {"success": True, "giria": giria}
         return {}
 
     @view_config(request_method="PUT", request_param="downvotes=true")
     def send_downvote(self):
-        """View responsavel pelo upvotes de um significado.
-        - 1 nos votos e adiciona o token do usuario em uma lista, onde existem
-        usuarios que ja votaram nesse significado."""
-        indice = self.req.json['description']
+        """View handler to save downvotes from `description`.
+        - 1 in `votes` and add user `token` a list `users`, where have only users
+        that votes it."""
+        index_ = self.req.json['description']
 
         state = State.objects(code=self.state).first()
         giria = Giria.objects(state=state, giria=self.giria).first()
 
         if giria:
 
-            giria.description[indice].votes['votes'] -= 1
-            giria.description[indice].votes['users'].append(self.token)
+            giria.description[index_].votes['votes'] -= 1
+            giria.description[index_].votes['users'].append(self.token)
             giria.save()
 
             self.sort_description(giria)
-
             return {"success": True, "giria": giria}
         return {}
 
     def sort_description(self, giria):
-        """Ordena os significados por votos"""
+        """Sort `descriptions` by `votes`"""
         giria.description = sorted(
             giria.description,
             key=lambda g: g.votes['votes'],
@@ -171,13 +168,12 @@ class GiriaItemView(object):
 
 
 def prepare_user_description(giria_info):
-    """Metodo usado para formatar o novo significado na collection de girias.
+    """Method used to format a new `description` in collection of `girias`.
     descriptions = {
-    'users': [lista de usuarios]
-    'votes': 9999
+        'users': [list of users-token]
+        'votes': 9999
     }
     """
-
     descriptions = GiriaDescription(
         description=giria_info["description"],
         votes={"users": set(), "votes": 0}
@@ -187,21 +183,20 @@ def prepare_user_description(giria_info):
 
 
 def encode_token(req, payload):
-    """Metodo usado para criar o token jwt. recebe um request e o payload com
-    informações do usuario. Por segurança, é removido a key `password`."""
+    """Method used to create the `token`. Receive request and the payload with
+    user info. By security, `password` key is removed from payload."""
     del payload["password"]
     return jwt.encode(payload, req.registry.settings.get("giriasecret", "galado"))
 
 
 def decode_token(req, jwt_str):
-    """Metodo usado para decodificar o token jwt. recebe um request e o token do
-    usuario."""
+    """Method used to decode the `token` JWT. Receive request and token jwt."""
     return jwt.decode(jwt_str, req.settings.get("giriasecret", "galado"))
 
 
 @view_config(route_name="signin", request_method="POST", renderer="json")
 def signin(request):
-    """View para criar novo usuario no sistema."""
+    """View used to create a user to system"""
     user_payload = request.json
 
     if User.objects(username=user_payload["username"]).first():
@@ -222,8 +217,7 @@ def signin(request):
 
 @view_config(route_name="login", request_method="POST", renderer="json")
 def login(request):
-    """View usada para login no sistema. Retorna o token do usuario, se ele
-    existir."""
+    """View used to login. Return a `token` of user logged, if exists"""
     user = User.objects(username=request.json.get("username")).first()
 
     if not user or not user.verify_password(request.json.get("password")):
